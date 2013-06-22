f3907e296ae163829a0cc27cb8f15b48c82c91a2
Matthew Scandalis <mscandal@Matthews-MacBook-Pro.local>
2013-06-22 13:47:22 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var sunlight = require('sunlight-congress-api');
+var express = require('express');
+var jade = require('jade');
+var when = require('when');
+
+
+var app = express();
+
+app.use(express.favicon());
+
+app.set('views', 'views');
+app.set('view engine', 'jade');
+
+app.use(express.static(__dirname + '/public'));
+
+var server = require('http').createServer(app);
+
+var port = process.env.PORT || 5000;
+server.listen(port);
